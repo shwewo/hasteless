55fe21d59a875571e9e4fcde14be8d66edabe0f7
shwewo <shwewo@gmail.com>
2024-03-06 19:59:55 +0300

    :3

--- flake.nix
+++ flake.nix
@@ -1,14 +1,30 @@
+# flake.nix
 {
-  description = "blog";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShells.default = import ./shell.nix { inherit pkgs; };
-        }
-      );
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [rust-overlay.overlays.default];
+    };
+    toolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      name = "hasteless";
+      packages = [
+        toolchain
+        pkgs.nodejs
+        pkgs.nodePackages.wrangler        
+      ];
+      PATH = [ "~/.cargo/bin" ];
+    };
+  };
 }
